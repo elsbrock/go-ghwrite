4a45cf9ce090fcc8f069241e181818b3ec4de416
Simon Elsbrock <simon@iodev.org>
2020-06-10 19:57:39 +0000

    chore: switch to Github Git API instead of CreateFile

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import "strings"
 import "fmt"
 import "io/ioutil"
 import "context"
+import "encoding/base64"
 import "github.com/google/go-github/v32/github"
 import "golang.org/x/oauth2"
 
@@ -13,7 +14,7 @@ var (
 	name      = flag.String("name", "Max Mustermann", "the author name")
 	email     = flag.String("email", "me@example.com", "the author email")
 	branch    = flag.String("branch", "master", "the git branch")
-	commitmsg = flag.String("commit-msg", "update submitted via go-ghwrite", "the commit message")
+	commitMsg = flag.String("commit-msg", "update submitted via go-ghwrite", "the commit message")
 )
 
 func usage() {
@@ -66,34 +67,78 @@ func main() {
 
 	client := github.NewClient(tc)
 
-	cfg, err := ioutil.ReadAll(os.Stdin)
+	repoSHA, _, err := client.Repositories.GetCommitSHA1(ctx, owner, repo, *branch, "")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	copts := &github.RepositoryContentGetOptions{}
-	fileContent, _, resp, err := client.Repositories.GetContents(ctx, owner, repo, destpath, copts)
-	if err != nil {
-		if resp == nil || (resp != nil && resp.StatusCode != 404) {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	sha := fileContent.GetSHA()
-
-	fopts := &github.RepositoryContentFileOptions{
-		Message:   commitmsg,
-		Content:   cfg,
-		SHA:       &sha,
-		Branch:    github.String(*branch),
-		Committer: &github.CommitAuthor{Name: github.String(*name), Email: github.String(*email)},
-	}
-
-	_, _, err = client.Repositories.CreateFile(ctx, owner, repo, destpath, fopts)
+	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	fblob := &github.Blob{}
+	b64c := "base64"
+	fblob.Encoding = &b64c
+	b64data := base64.StdEncoding.EncodeToString(data)
+	fblob.Content = &b64data
+
+	blob, _, err := client.Git.CreateBlob(ctx, owner, repo, fblob)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	entries := make([]*github.TreeEntry, 0, 0)
+	fileSHA := blob.GetSHA()
+	mode := "100644"
+	blobType := "blob"
+	entry := github.TreeEntry{
+		SHA:  &fileSHA,
+		Path: &destpath,
+		Mode: &mode,
+		Type: &blobType,
+	}
+	entries = append(entries, &entry)
+
+	tree, _, err := client.Git.CreateTree(ctx, owner, repo, repoSHA, entries)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	commit := &github.Commit{}
+	author := &github.CommitAuthor{}
+	author.Name = name
+	author.Email = email
+	commit.Author = author
+	commit.Message = commitMsg
+	commit.Tree = tree
+	c := github.Commit{}
+	c.SHA = &repoSHA
+	commit.Parents = []*github.Commit{&c}
+
+	newCommit, _, err := client.Git.CreateCommit(ctx, owner, repo, commit)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ref := &github.Reference{}
+	branchRef := fmt.Sprintf("refs/heads/%s", *branch)
+	ref.Ref = &branchRef
+	obj := &github.GitObject{}
+	commitType := "commit"
+	obj.Type = &commitType
+	commitSHA := newCommit.GetSHA()
+	obj.SHA = &commitSHA
+	ref.Object = obj
+	_, _, err = client.Git.UpdateRef(ctx, owner, repo, ref, false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprintf(os.Stderr, "committed as %s\n", commitSHA)
 }
